03b26d67e1e182b11a6cd62e8c5a43c76282ba61
juemimgcd <3489161689@qq.com>
2026-07-15 21:11:16 +0800

    fix agent migration revision chain

--- alembic/versions/20260715_01_add_agent_tool_evidence.py
+++ alembic/versions/20260715_01_add_agent_tool_evidence.py
@@ -1,7 +1,7 @@
 """add agent tool evidence to chat messages
 
-Revision ID: 20260715_01
-Revises: 20260713_01
+Revision ID: 20260715_04
+Revises: 20260715_03
 Create Date: 2026-07-15 00:00:00.000000
 
 """
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 from alembic import op
 
-revision = "20260715_01"
-down_revision = "20260713_01"
+revision = "20260715_04"
+down_revision = "20260715_03"
 branch_labels = None
 depends_on = None
 
